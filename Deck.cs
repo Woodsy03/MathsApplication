f56a57465dd3c680d4109ef062d0928a949203b6
Woodsy03 <119528792+Woodsy03@users.noreply.github.com>
2023-04-19 20:01:14 +0100

    Add project files.

---
+++ Deck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Deck
+{
+    private List<string> Cards { get; set; }
+    public Deck()
+    {
+        GenerateDeck();
+        Shuffle();
+    }
+
+    private void GenerateDeck()
+    {
+        List<string> numbers = new List<string>() { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
+        List<string> suites = new List<string>() { "Hearts", "Spades", "Clubs", "Diamonds" };
+
+        Cards = new List<string>();
+        foreach (string suite in suites)
+            foreach (string number in numbers)
+                Cards.Add(number + " of " + suite);
+    }
+
+    private void Shuffle()
+    {
+        Random rnd = new Random();
+        for (int i = Cards.Count - 1; i > 0; i--)
+        {
+            int randomIndex = rnd.Next(0, i + 1);
+            string temp = Cards[i];
+            Cards[i] = Cards[randomIndex];
+            Cards[randomIndex] = temp;
+        }
+    }
+
+}
---
+++ Tutorial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Tutorial
+{
+    public void Introduction()
+    {
+        Console.WriteLine("Hi there! Welcome to your personal maths learning game!");
+        Console.WriteLine("Here's how you play our Maths game.");
+
+    }
+
+    public void Exit()
+    {
+        Console.WriteLine("Thank you for playing! Goodbye.");
+    }
+
+}
