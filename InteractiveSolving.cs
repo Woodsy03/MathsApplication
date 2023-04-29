16a2e2d4e9f3774a029b32175ac162d8e420feca
Woodsy03 <119528792+Woodsy03@users.noreply.github.com>
2023-04-29 17:48:06 +0100

    Interactive solving class beta completion, added answer solving and error checking

--- InteractiveSolving.cs
+++ InteractiveSolving.cs
@@ -11,8 +11,6 @@ namespace MathsApplication
 
         public void Quiz(Stacks stacks)
         {
-            
-
             Console.WriteLine("Please solve the following");
             if (stacks.OperandStack[0] == "/") //targeted at 6-7 year olds so all decimals have been removed
             {
@@ -20,12 +18,52 @@ namespace MathsApplication
                 {
                     stacks.NumberStack.RemoveAt(0);
                 }
+            int correcttAnswer = AnswerCalculation(stacks);
             }
+        }
 
-            Console.WriteLine(stacks.NumberStack[0] + " " + stacks.OperandStack[0] + " " + stacks.NumberStack[1]);
-            string UserAnswer = Console.ReadLine();
-            //while (answer != )
+        private int AnswerCalculation(Stacks stacks)
+        {
+            int CorrectAnswer = 0;
+            switch (stacks.OperandStack[0])
+            {
+                case "+":
+                    CorrectAnswer = stacks.NumberStack[0] + stacks.NumberStack[1];
+                    break;
+                case "-":
+                    CorrectAnswer = stacks.NumberStack[0] - stacks.NumberStack[1];
+                    break;
+                case "*":
+                    CorrectAnswer = stacks.NumberStack[0] * stacks.NumberStack[1];
+                    break;
+                case "/":
+                    CorrectAnswer = stacks.NumberStack[0] / stacks.NumberStack[1];
+                    break;
+            }
+            return CorrectAnswer;
+        }
 
+        internal void QuizRecursion()
+        {
+            Console.WriteLine("would you like to play again? Yes/No");
+            string PlayAgain = Console.ReadLine();
+            PlayAgain = PlayAgain.ToLower();
+
+            if (PlayAgain == "yes")
+            {
+                Stacks stacks = new Stacks();
+                Quiz(stacks);
+            }
+            else if (PlayAgain == "no")
+            {
+                Console.WriteLine("Thanks for playing! Returning to menu.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+                QuizRecursion();
+            }
         }
 
 
